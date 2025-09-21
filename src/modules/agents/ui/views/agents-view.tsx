bd20eb63d6fbb3b944e03a3a4bd292d491dc151b
Shubhangam <shubhangampandey1@gmail.com>
2025-09-21 16:22:28 +0530

    a

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -4,11 +4,23 @@ import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { DataTable } from "../components/data-table";
+import { columns, Payment } from "../components/columns";
+const mockData : Payment[] =[
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+  ]
 
 const AgentsView = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+  return <div>
+    <DataTable data={mockData} columns={columns}/>
+  </div>;
 };
 
 export const AgentsViewLoading = () => {
