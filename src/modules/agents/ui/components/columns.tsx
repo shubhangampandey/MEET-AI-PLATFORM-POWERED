4ae6307832b01589dedafafd4dc523c51db33975
Shubhangam <shubhangampandey1@gmail.com>
2025-09-21 17:21:29 +0530

    g

--- src/modules/agents/ui/components/columns.tsx
+++ src/modules/agents/ui/components/columns.tsx
@@ -2,6 +2,10 @@
 
 import { ColumnDef } from "@tanstack/react-table"
 import { AgentGetOne } from "../../types"
+import GenerateAvatar from "@/components/generate-avatar"
+import { Badge} from "@/components/ui/badge"
+import { access } from "fs"
+import { VideoIcon, CornerDownRightIcon } from "lucide-react"
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -11,6 +15,38 @@ export const columns: ColumnDef<AgentGetOne>[] = [
   {
     accessorKey: "name",
     header: "Agent Name",
+    cell: ({ row }) => 
+    <div className="flex flex-col gap-y-1">
+        <div className="flex items-center gap-x-2 ">
+          <GenerateAvatar seed={row.original.name} 
+          variant="botttsNeutral" 
+          className="size-6"/>
+          <span className="font-semibold capitalize">{row.original.name}</span>
+        </div>
+       
+            <div className="flex items-centre gap-x-2">
+                <CornerDownRightIcon className="text-3 text-muted-foreground max-w-[200px] truncate capitalize"/>
+                <span className="text-sm text-muted-foreground">
+                    {row.original.instructions} </span>
+                </div>
+        </div>
+   
+    
   },
-  
+  {
+    accessorKey: "meetingCount",
+    header: "Meetings",
+    cell: ({ row }) => (
+      
+        <Badge
+          variant="outline"
+          className="flex items-center gap-x-2 [&>svg]:size-4"
+        >
+          <VideoIcon className="text-blue-700" />
+          {row.original.meetingCount}
+          {row.original.meetingCount === 1 ? "meeting" : "meetings"}
+        </Badge>
+      
+    )
+  }
 ]
