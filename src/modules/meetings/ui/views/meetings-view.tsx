60e5faf7bf418ecaab12345bc4f1d4c941e5f778
Shubhangam <shubhangampandey1@gmail.com>
2025-09-25 12:59:08 +0530

    a

--- src/modules/meetings/ui/views/meetings-view.tsx
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -1,11 +1,11 @@
 "use client";
 import {useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
+import {  useSuspenseQuery } from "@tanstack/react-query";
 import { LoadingState } from "@/components/loading-state";
 import { ErrorState } from "@/components/error-state";
 export const MeetingsView = () => {
 const trpc = useTRPC();
-const {data } = useQuery (trpc.meetings.getMany.queryOptions({}));
+const {data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
 return (
   <div>
     {JSON.stringify(data)}
