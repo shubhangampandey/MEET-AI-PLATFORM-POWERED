34f71fbd444778c5dabf65e0ffc8b7638e240d10
Shubhangam <shubhangampandey1@gmail.com>
2025-09-26 21:07:33 +0530

    a

--- src/components/command-select.tsx
+++ src/components/command-select.tsx
@@ -32,6 +32,10 @@ export const CommandSelect = ({
 }: Props)=>{
   const [open, setOpen] = useState(false);
   const selectedOption = options.find((option) => option.value === value);
+  const handleOpenChange=(value: boolean)=>{
+    onSearch?.("");
+    setOpen(value);
+  }
   return (
       <>
       <Button
@@ -53,7 +57,7 @@ export const CommandSelect = ({
       <CommandResponsiveDialog
       shouldFilter={!onSearch} 
       open={open}
-      onOpenChange={setOpen}
+      onOpenChange={handleOpenChange}
       >
         <CommandInput placeholder="Search..." onValueChange={onSearch} />
         <CommandList>
--- src/modules/meetings/types.ts
+++ src/modules/meetings/types.ts
@@ -4,3 +4,10 @@ import type { AppRouter } from "@/trpc/routers/_app";
 
 export type MeetingGetMany = inferRouterOutputs<AppRouter>["meetings"]["getMany"]["items"];
 export type MeetingGetOne = inferRouterOutputs<AppRouter>["meetings"]["getOne"];
+export enum MeetingStatus {
+   Upcoming = "upcoming",
+   Active = "active",
+   Processing = "processing",
+   Completed = "completed",
+   Cancelled = "cancelled",
+}
